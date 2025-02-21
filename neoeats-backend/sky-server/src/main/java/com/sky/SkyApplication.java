fc35706f37372b84a7cde6349325c27d54504488
hundred-henry <onehundred.hongji@gmail.com>
2025-02-21 01:54:38 -0600

    Ali uploadImg

--- neoeats-backend/sky-server/src/main/java/com/sky/SkyApplication.java
+++ neoeats-backend/sky-server/src/main/java/com/sky/SkyApplication.java
@@ -1,5 +1,6 @@
 package com.sky;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -10,6 +11,15 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 @Slf4j
 public class SkyApplication {
     public static void main(String[] args) {
+        Dotenv dotenv = Dotenv.configure()
+                .directory("sky-server/src/main/resources")
+                .load();
+
+        // 例如将 .env 中的变量设置到系统属性中
+        System.setProperty("DB_PASSWORD", dotenv.get("DB_PASSWORD"));
+        System.setProperty("ALI_ACCESS_KEY_ID", dotenv.get("ALI_ACCESS_KEY_ID"));
+        System.setProperty("ALI_ACCESS_KEY_SECRET", dotenv.get("ALI_ACCESS_KEY_SECRET"));
+
         SpringApplication.run(SkyApplication.class, args);
         log.info("server started");
     }
