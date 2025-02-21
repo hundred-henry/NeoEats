77b8c08b273abf85ebbdd0375bd74d540431d30f
hundred-henry <onehundred.hongji@gmail.com>
2025-02-21 16:20:45 -0600

    implement deleteDish

--- neoeats-backend/sky-server/src/main/java/com/sky/controller/admin/DishController.java
+++ neoeats-backend/sky-server/src/main/java/com/sky/controller/admin/DishController.java
@@ -11,6 +11,8 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("/admin/dish")
 @Api("菜品相关接口")
@@ -35,5 +37,12 @@ public class DishController {
     public Result<PageResult> page(DishPageQueryDTO dishPageQueryDTO) {
         PageResult pageResult = dishService.pageQuery(dishPageQueryDTO);
         return Result.success(pageResult);
+    }
+
+    @DeleteMapping
+    @ApiOperation("菜品批量删除")
+    public Result delete(@RequestParam List<Long> ids) {
+        dishService.deleteBatch(ids);
+        return Result.success();
     }
 }
