0b37dfc6a11cbeb10077274a0b0ca4b5aa141bec
hundred-henry <onehundred.hongji@gmail.com>
2025-02-20 20:11:00 -0600

    implement get and update employee

--- neoeats-backend/sky-server/src/main/java/com/sky/service/EmployeeService.java
+++ neoeats-backend/sky-server/src/main/java/com/sky/service/EmployeeService.java
@@ -22,4 +22,24 @@ public interface EmployeeService {
     void save(EmployeeDTO employeeDTO);
 
     PageResult pageQuery(EmployeePageQueryDTO employeePageQueryDTO);
+
+    /**
+     *
+     * @param status
+     * @param id
+     */
+    void startOrStop(Integer status, Long id);
+
+    /**
+     * 根据id查询员工信息
+     * @param id
+     * @return
+     */
+    Employee getById(Long id);
+
+    /**
+     * 编辑员工信息
+     * @param employeeDTO
+     */
+    void updateById(EmployeeDTO employeeDTO);
 }
