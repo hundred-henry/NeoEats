5e01ba72aa7952f22fb2db9a9a7b4fb2ac15692a
hundred-henry <onehundred.hongji@gmail.com>
2025-02-23 14:51:18 -0600

    implement address

--- neoeats-backend/sky-server/src/main/java/com/sky/mapper/ShoppingCartMapper.java
+++ neoeats-backend/sky-server/src/main/java/com/sky/mapper/ShoppingCartMapper.java
@@ -1,6 +1,7 @@
 package com.sky.mapper;
 
 import com.sky.entity.ShoppingCart;
+import org.apache.ibatis.annotations.Delete;
 import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Update;
@@ -23,4 +24,7 @@ public interface ShoppingCartMapper {
     @Insert("INSERT INTO shopping_cart(name, user_id, dish_id, setmeal_id, dish_flavor, number, amount, image, create_time) " +
             "VALUES (#{name}, #{userId}, #{dishId}, #{setmealId}, #{dishFlavor}, #{number}, #{amount}, #{image}, #{createTime})")
     void insert(ShoppingCart shoppingCart);
+
+    @Delete("DELETE FROM shopping_cart WHERE user_id = #{userId}")
+    void deleteByUserId(Long userId);
 }
--- neoeats-backend/sky-server/src/main/java/com/sky/service/impl/ShoppingCartServiceImpl.java
+++ neoeats-backend/sky-server/src/main/java/com/sky/service/impl/ShoppingCartServiceImpl.java
@@ -72,4 +72,9 @@ public class ShoppingCartServiceImpl implements ShoppingCartService {
         List<ShoppingCart> list = shoppingCartMapper.list(shoppingCart);
         return list;
     }
+
+    public void cleanShoppingCart() {
+        Long userId = BaseContext.getCurrentId();
+        shoppingCartMapper.deleteByUserId(userId);
+    }
 }
