32f0661852ce22c7cbe187202ee8c36db7e7c6c2
hundred-henry <onehundred.hongji@gmail.com>
2025-02-24 10:01:20 -0600

    user order

--- neoeats-backend/sky-server/src/main/java/com/sky/service/OrderService.java
+++ neoeats-backend/sky-server/src/main/java/com/sky/service/OrderService.java
@@ -1,9 +1,12 @@
 package com.sky.service;
 
+import com.sky.dto.OrdersPageQueryDTO;
 import com.sky.dto.OrdersPaymentDTO;
 import com.sky.dto.OrdersSubmitDTO;
+import com.sky.result.PageResult;
 import com.sky.vo.OrderPaymentVO;
 import com.sky.vo.OrderSubmitVO;
+import com.sky.vo.OrderVO;
 
 public interface OrderService {
 
@@ -26,4 +29,14 @@ public interface OrderService {
      * @param outTradeNo
      */
     void paySuccess(String outTradeNo);
+
+    PageResult pageQuery4User(int page, int pageSize, Integer status);
+
+    OrderVO details(Long id);
+
+    void userCancelById(Long id) throws Exception;
+
+    void repetition(Long id);
+
+    PageResult conditionSearch(OrdersPageQueryDTO ordersPageQueryDTO);
 }
