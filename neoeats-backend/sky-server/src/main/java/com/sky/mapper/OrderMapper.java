9e79ea31eb791fb33083497ae9811ba805658275
hundred-henry <onehundred.hongji@gmail.com>
2025-02-25 11:06:59 -0600

    sales Top10

--- neoeats-backend/sky-server/src/main/java/com/sky/mapper/OrderMapper.java
+++ neoeats-backend/sky-server/src/main/java/com/sky/mapper/OrderMapper.java
@@ -1,6 +1,7 @@
 package com.sky.mapper;
 
 import com.github.pagehelper.Page;
+import com.sky.dto.GoodsSalesDTO;
 import com.sky.dto.OrdersPageQueryDTO;
 import com.sky.entity.Orders;
 import org.apache.ibatis.annotations.Mapper;
@@ -65,4 +66,6 @@ public interface OrderMapper {
      * @return
      */
     Integer countByMap(Map map);
+
+    List<GoodsSalesDTO> getSalesTop10(LocalDateTime beginTime, LocalDateTime endTime, Integer status);
 }
